971f6c9211e27dedc9cb0afc53d42717dafc6b51
Richard Ash <r.ash8347@gmail.com>
2023-07-19 13:20:54 -0700

    Finish through More Tidying

--- elixir/elixir-for-programmers/hangman/html_client/lib/html_client_web/controllers/hangman_html.ex
+++ elixir/elixir-for-programmers/hangman/html_client/lib/html_client_web/controllers/hangman_html.ex
@@ -4,8 +4,37 @@ defmodule HtmlClientWeb.HangmanHTML do
 
   embed_templates "hangman_html/*"
 
+
+  @status_fields %{
+    initializing: { "initializing", "Guess the word, one letter at a time" },
+    good_guess: {"good-guess", "Good guess!"},
+    bad_guess: {"bad-guess", "Bad guess!"},
+    won: {"won", "You won!"},
+    lost: {"lost", "You lost!"},
+    already_guessed: {"already-guessed", "You already guessed that letter"},
+  }
+
   def home do
     ~p"/hangman"
+  end
+
+  def move_status(status) do
+    { class, msg } = @status_fields[status]
+    "<div class='status #{class}'>#{msg}</div>"
+  end
+
+  def continue_or_try_again(_conn, status) when status in [:won, :lost] do
+    button("New game", to: home(), method: :post)
+  end
+
+  def continue_or_try_again(conn, _) do
+    form_for(conn, home(), [ as: "make_move", method: :put ], fn f ->
+      [
+        text_input(f, :guess),
+        " ",
+        submit("Make a guess")
+      ]
+    end)
   end
 
   def figure_for(0) do
